c35202f9dbd12693e6e4b787ea2c0fce1a48f2ab
Jason Qiu <jasonblankqiu@gmail.com>
2020-09-30 15:17:35 -0500

    Iteration 1 Complete: accessing the webpage runs a hard-coded constraint system through Racket and displays it for the client

---
+++ project/CalcWrapper.py
@@ -0,0 +1,52 @@
+import subprocess, re, pathlib, os
+
+def calc():
+    filePath = os.path.join(pathlib.Path(__file__).parent.absolute(),'racket', 'constraintsys1.rkt')
+    testFile = open(filePath,'w')
+    
+    # hardcoded string
+    # will be built later through recursion (like derivative calc)
+    codeOut = """
+    ;; First two lines won't change
+    #lang racket
+    (require \"ConstraintLogic.rkt\")
+    
+    ;; (define (func...
+    ;; include a letter for each unknown var
+    (define (celsius-fahrenheit-converter c f)
+    ;; connector for each layer of calc (parenthesis)
+      (let ((u (make-connector))
+            (v (make-connector))
+            (w (make-connector))
+            (x (make-connector))
+            (y (make-connector)))
+        ;; use operators to define constraints between layers
+        (multiplier c w u)
+        (multiplier v x u)
+        (adder v y f)
+        (constant 9 w)
+        (constant 5 x)
+        (constant 32 y)
+        'ok))
+    
+    ;; make connectors for unknown vars
+    (define C (make-connector))
+    (define F (make-connector))
+    (celsius-fahrenheit-converter C F)
+    ;; user must set values for n-1 vars
+    (set-value! C 100 'user)
+    
+    ;; get value for unknown var (nth)
+    ;; wrap answer in '[ ]'
+    (display (string-append "["
+                            (number->string (get-value F))
+                            "]"))
+    ;; TEST
+    """
+    
+    testFile.write(codeOut)
+    testFile.close()
+    
+    calcOut = str(subprocess.run(['/usr/bin/racket', filePath],
+            stdout=subprocess.PIPE).stdout)
+    return(re.search('\[([^]]+)', calcOut).group(1))
--- project/views.py
+++ project/views.py
@@ -1,7 +1,18 @@
 from . import app
-from flask import render_template
+from flask import render_template, request
+from .CalcWrapper import calc
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+#Home Page of the Website
+#Form with two boxes "a" and "b"
+#GET method is default when loading a page
+#Once submitted, "POST" method is used
+@app.route("/", methods = ["POST", "GET"])
+def home():
+    #if the method that gets called is POST, pass values to compute()
+    if request.method == "POST":
+        a, b = request.form["a"], request.form["b"]
+        return f"<h1>{calc()}<h1>"
+    #else display calc page
+    else:
+        return render_template("calc.html")
 
