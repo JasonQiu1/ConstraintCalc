5a5d3cc820c07899ca8ab5f401b3792db4255888
Jason Qiu <jasonblankqiu@gmail.com>
2020-11-22 16:27:54 -0600

    Iteration 2 Complete: Added constraint system code parser and added the eval. Can now evaluate expressions from the command line and return the answer stored in 'ans

--- project/racket/ConstraintLogic.rkt
+++ project/racket/ConstraintLogic.rkt
@@ -1,4 +1,15 @@
 #lang racket
+
+; cmdline arg with default val
+(define in-expr (make-parameter '()))
+
+; cmdline parser
+(define parser
+    (command-line
+         #:args (expr)
+         (with-input-from-string expr read)))
+
+; constraint net code
 (define (for-each-except exception procedure list)
   (define (loop items)
     (cond ((null? items) 'done)
@@ -33,7 +44,7 @@
           'ignored))
     (define (connect new-constraint)
       (unless (memq new-constraint constraints)
-          (set! constraints 
+          (set! constraints
                 (cons new-constraint constraints)))
       (when (has-value? me)
           (inform-about-value new-constraint))
@@ -76,11 +87,11 @@
       (forget-value! operand me)
       (process-new-value))
     (define (me request)
-      (cond ((eq? request 'I-have-a-value)  
+      (cond ((eq? request 'I-have-a-value)
              (process-new-value))
-            ((eq? request 'I-lost-my-value) 
+            ((eq? request 'I-lost-my-value)
              (process-forget-value))
-            (else 
+            (else
              (error (string-append "Unknown request -- " constraint-name) request))))
     (connect operand me)
     (connect result me)
@@ -107,17 +118,17 @@
       (forget-value! operand2 me)
       (process-new-value))
     (define (me request)
-      (cond ((eq? request 'I-have-a-value)  
+      (cond ((eq? request 'I-have-a-value)
              (process-new-value))
-            ((eq? request 'I-lost-my-value) 
+            ((eq? request 'I-lost-my-value)
              (process-forget-value))
-            (else 
+            (else
              (error (string-append "Unknown request -- " constraint-name) request))))
     (connect operand1 me)
     (connect operand2 me)
     (connect result me)
     me))
-  
+
 (define adder (make-binary-constraint (lambda (a1 a2) (+ (get-value a1) (get-value a2)))
                                       (lambda (a1 sum) (- (get-value sum) (get-value a1)))
                                       (lambda (a2 sum) (- (get-value sum) (get-value a2)))
@@ -159,73 +170,79 @@
 ;; accessors for prefix expressions
 (define (get-op expr)
   (if (> (length expr) 0)
-      (car expr)
-      '()))
-	
+  	  (car expr)
+  	  '()))
+
 (define (get-sub1 expr)
   (if (> (length expr) 1)
-      (cadr expr)
-      '()))
-	
+  	  (cadr expr)
+  	  '()))
+
 (define (get-sub2 expr)
   (if (> (length expr) 2)
-      (caddr expr)
-      '()))
-	
+  	  (caddr expr)
+  	  '()))
+
 ; return unused name or "ans" if "ans" is found in expr
 (define next-list-name
   (let ([count 0])
-    (lambda (expr)
-      (if (or (eq? (get-op expr) "ans") (null? (get-op expr)))	; returning "ans" will do nothing for nil cases
-          "ans"
-          (begin (set! count (+ count 1))
-                 (string-append "l" (number->string count)))))))
-	
-(define op-symbol-table #hash(("+" . "adder")
-                              ("-" . "adder")
-                              ("*" . "multiplier")
-                              ("const" . "constant")))
-	
+  	(lambda (expr)
+  		(if (or (eq? (get-op expr) 'ans) (null? (get-op expr)))	; returning "ans" will do nothing for nil cases
+  			'ans
+  			(begin (set! count (+ count 1))
+  		   	   	   (string->symbol (string-append "l" (number->string count))))))))
+
+(define op-symbol-table #hash((+ . adder)
+							  (- . adder)
+                              (* . multiplier)
+                              (/ . multiplier)
+                              (const . constant)))
+
 ; future: cond analysis to handle unary, binary, constants
 (define (make-constraint-list op sub1-name sub2-name layer-name)
   (list (hash-ref op-symbol-table op) sub1-name sub2-name layer-name))
-	
-;;(define (get-constraint-code expr names)
-;;  	(cond 	[(eq? (car expr) '+) (append (list (list 'adder (cadr names) (caddr names) layer-name)) (list (get-constraint-code (cadr expr) (cadr names) (cdr names)))) (list (get-constraint-code (caddr expr) (car names) (cdr names))))]
-;;			[else (list 'constant (car expr) layer-name)]))
-	
+
 ; constrains right-hand side to left-hand side of eqtn by sharing the top-layer name
-(define (get-constraint-code-equaler expr top-layer-name)
-  (let ([lhs (get-constraint-code (cadr expr) top-layer-name)]
-        [rhs (get-constraint-code (caddr expr) top-layer-name)])
-    (append lhs rhs)))
-	
+(define (get-constraint-list-equaler expr top-layer-name)
+  (let ([lhs (get-constraint-list (cadr expr) top-layer-name)]
+  		[rhs (get-constraint-list (caddr expr) top-layer-name)])
+	(append lhs rhs)))
+
 ; build list of constraint code for each layer and its subexpressions/sublayers
 ; general case (binary op): (op sub1 sub2)
 ; unary op: 				(op sub1)
 ; constant:					(op)
-(define (get-constraint-code expr current-layer)
+(define (get-constraint-list expr current-layer)
   (let* ([op (get-op expr)]			; may be a constant
-         [sub1 (get-sub1 expr)]
-         [sub2 (get-sub2 expr)]
-         [sub1-name (next-list-name sub1)]
-         [sub2-name (next-list-name sub2)])
-    (cond [(eq? op "+") (append (list (make-constraint-list op sub1-name sub2-name current-layer))
-                                (get-constraint-code sub1 sub1-name)
-                                (get-constraint-code sub2 sub2-name))]
-          [(eq? op "-") (append (list (make-constraint-list op current-layer sub2-name sub1-name))
-                                (get-constraint-code sub1 sub1-name)
-                                (get-constraint-code sub2 sub2-name))]
-          [(number? op) (list (list "constant" op current-layer))]
-          [(eq? op "ans") '()])))	; assume an "ans" by itself is (+ (0) (ans))
+  		 [sub1 (get-sub1 expr)]
+  		 [sub2 (get-sub2 expr)]
+  		 [sub1-name (next-list-name sub1)]
+  		 [sub2-name (next-list-name sub2)])
+  	(cond [(eq? op '+) (append (list (make-constraint-list op sub1-name sub2-name current-layer))
+  								(get-constraint-list sub1 sub1-name)
+  								(get-constraint-list sub2 sub2-name))]
+  		  [(eq? op '-) (append (list (make-constraint-list op current-layer sub2-name sub1-name))
+  								(get-constraint-list sub1 sub1-name)
+  								(get-constraint-list sub2 sub2-name))]
+ 	 	  [(eq? op '*) (append (list (make-constraint-list op sub1-name sub2-name current-layer))
+  								(get-constraint-list sub1 sub1-name)
+  								(get-constraint-list sub2 sub2-name))]
+  		  [(eq? op '/) (append (list (make-constraint-list op current-layer sub2-name sub1-name))
+  								(get-constraint-list sub1 sub1-name)
+  								(get-constraint-list sub2 sub2-name))]
+  		  [(number? op) (list (list 'constant op current-layer))]
+  		  [(eq? op 'ans) '()])))	; assume an "ans" by itself is (+ (0) (ans))
 
+; code parser procedures
+; grabs the unique connector name symbols from the list of constraint code obtained from get-constraint-list
 (define (constraint-list->connector-name-list constraint-list)
   (define (iter list)
-    (cond [(or (null? list)) (set)]
+    (cond [(null? list) (set)]
           [(list? (car list)) (set-union (iter (car list)) (iter (cdr list)))]
-          [else (list->set (filter string? list))]))
+          [else (list->set (filter symbol? (cdr list)))]))
   (set->list (iter constraint-list)))
 
+; converts the list of constraint code to a string able to be 'eval'uated
 (define (constraint-list->code-string constraint-list)
   (substring (foldr string-append
                     ""
@@ -242,21 +259,44 @@
                          constraint-list))
              1))
 
-(define (make-constraint-system-code-string con-sys-name connector-name-list constraint-code-string)
+;; (define generic-constraint-system
+;;   (let ((ans (make-connector))
+;;   		(l1 (make-connector))
+;;         (l2 (make-connector))
+;;         (l3 (make-connector))
+;;         (l4 (make-connector))
+;;         (l5 (make-connector))
+;;         (l6 (make-connector)))
+;;     (multiplier l6 l3 l1)
+;;     (multiplier l2 l4 l1)
+;;     (adder l2 l5 ans)
+;;     (constant 9 l3)
+;;     (constant 32 l5)
+;;     (constant 5 l4)
+;;     (constant 100.888 l6)
+;;     (get-value ans)))
+;;
+;; generic-constraint-system
+
+; parses the entire constraint system together as a string able to be 'eval'uated given a list of constraint relationships
+(define (make-constraint-system-code-string con-sys-name constraint-list)
+  (string? list? . -> . string?)
   (define make-connectors
     (substring (foldr string-append
                       ""
-                      (map (lambda (l) (string-append " (" l " (make-connector))" ))
-                           connector-name-list))
+                      (map (lambda (l) (string-append " (" (symbol->string l) " (make-connector))" ))
+                           (constraint-list->connector-name-list constraint-list)))
                1))
-  (string-append "(define "
+  (string-append "(begin (define "
                  con-sys-name
                  " (let ("
                  make-connectors
                  ") "
-                 constraint-code-string
-                 " (get-value ans)))"))
+                 (constraint-list->code-string constraint-list)
+                 " (get-value ans))) "
+                 con-sys-name ")"))
 
-(define constraint-table #hash(("+" . 'adder)
-                               ("*" . 'multiplier)
-                               ("=" . 'equaler)))
+; eval the code and return the answer
+(define-namespace-anchor a)
+(define ns (namespace-anchor->namespace a))
+(eval (call-with-input-string (make-constraint-system-code-string "TEST" (get-constraint-list-equaler parser 'top)) read) ns)
